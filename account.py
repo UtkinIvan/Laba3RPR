22e1f7f8c38d3cb7e5735d30562594042662949e
UtkinIvan <95709064+UtkinIvan@users.noreply.github.com>
2023-09-08 11:45:22 +0300

    Add files via upload

---
+++ account.py
@@ -0,0 +1,14 @@
+import value as value
+
+
+class Account(object):
+    def __init__(self,name, balance):
+        self.name = name
+        self.balance = balance
+
+    def deposit(self, balance):
+        self.balance += value
+
+if __name__ == '__main__':
+  acc = Account('a',100)
+  acc.deposit(10)
